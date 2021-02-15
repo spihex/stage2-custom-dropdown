3ff44eb722c5f7f38c3b2b18aebc4d767eae90e9
spihex <33398695+spihex@users.noreply.github.com>
2021-02-15 17:00:33 +1000

    теперь с использованием класса

--- js/main.js
+++ js/main.js
@@ -1,113 +1,132 @@
-let dropdownInputField = document.querySelector('.dropdown__input');
-let dropdownList = document.querySelector('.dropdown__list');
-let dropdown = document.querySelector('.dropdown')
-let previousValue;
-let dropdownListItemsValues = [];
+class Dropdown {
 
-document.addEventListener("DOMContentLoaded", function () {
-    fetch('data.json')
-        .then(response => response.json())
-        .then(data => {
-            dropdownList.innerHTML = '';
+    constructor(options) {
 
-            data.forEach(item => {
-                dropdownList.innerHTML += ` <li class="dropdown__item">${item.label}</li>`;
-                dropdownListItemsValues.push(item.label);
-            });
+        this.selector = options.container;
+        this.container = document.querySelector(options.container);
+        this.dropdownInputField = document.querySelector(this.selector + ' .dropdown__input');
 
-            dropdownListItems = document.querySelectorAll('.dropdown__item');
-
-        })
-});
+        this.dropdownList = document.querySelector(this.selector + ' .dropdown__list');
+        this.dataSource = options.dataSource;
+        this.previousValue;
+        this.dropdownListItemsValues = [];
+        this.dropdownListItems;
 
 
-document.addEventListener('mousedown', (e) => {
-    let isDropdown = dropdownList.contains(e.target);
-    let isInput = dropdownInputField.contains(e.target);
-    if (!isDropdown && !isInput) {
-        dropdownList.classList.remove('open');
+        this.container.addEventListener('mousedown', this.onMouseDown.bind(this));
+        this.dropdownInputField.addEventListener('input', this.onInput.bind(this));
+        this.dropdownInputField.addEventListener('blur', this.onBlur.bind(this));
+        this.dropdownInputField.addEventListener('focus', this.onFocus.bind(this));
+
+        this.init(this.dataSource);
+
     }
 
-    if (e.button === 0) {
+    init(source) {
 
-        if (e.target.classList.contains('dropdown__item')) {
-            previousValue = e.target.textContent;
-            dropdownInputField.value = e.target.textContent;
+        fetch(source)
+            .then(response => response.json())
+            .then(data => {
+                this.dropdownList.innerHTML = '';
+                data.forEach(item => {
+                    this.dropdownList.innerHTML += ` <li class="dropdown__item">${item.label}</li>`;
+                    this.dropdownListItemsValues.push(item.label);
+                });
 
-            dropdownListItems.forEach(dropdownList => {
-                dropdownList.classList.add('closed');
-            });
+                this.dropdownListItems = document.querySelectorAll(this.selector + ' .dropdown__item');
+
+            })
+
+        window.addEventListener('resize', function() {
+            this.dropdownInputField.blur();
+        });
+
+        window.addEventListener('scroll', function() {
+            this.dropdownInputField.blur();
+        });
+
+    }
+
+    onFocus() {
+        this.dropdownInputField.placeholder = 'Напиши что-нибудь';
+        this.dropdownInputField.value = '';
+        this.dropdownList.classList.add('open');
+        this.dropdownListItems.forEach(dropdownList => {
+            dropdownList.classList.remove('closed');
+        });
+
+        if ((this.container.offsetTop) + 220 >= window.innerHeight) {
+            //220 - minimum height of dropdown list
+
+            this.dropdownList.style.top = 'initial';
+            this.dropdownList.style.bottom = '0';
+
+        } else {
+
+            this.dropdownList.style.top = '0';
+            this.dropdownList.style.bottom = 'initial';
+
         }
-
     }
-});
 
-dropdownInputField.addEventListener('input', () => {
-    dropdownList.classList.add('open');
+    onBlur() {
+        this.dropdownInputField.placeholder = 'Выбери что-нибудь';
+        this.dropdownList.classList.remove('open');
 
-    let inputValue = dropdownInputField.value.toLowerCase();
-    if (inputValue.length > 0) {
-        for (let i = 0; i < dropdownListItemsValues.length; i++) {
-            if (!(inputValue.substring(0, inputValue.length) === dropdownListItemsValues[i].substring(0, inputValue.length).toLowerCase())) {
-                dropdownListItems[i].classList.add('closed');
-            } else {
-                dropdownListItems[i].classList.remove('closed');
+        let dropdownInputFieldValue = this.dropdownInputField.value;
+
+        let currentValueMatches = this.dropdownListItemsValues.filter(function(item) {
+            return item.toLowerCase() == dropdownInputFieldValue.toLowerCase();
+        });
+
+        if (currentValueMatches.length < 1) {
+            this.dropdownInputField.value = '';
+
+        }
+        if (this.previousValue)
+            this.dropdownInputField.value = this.previousValue;
+    }
+
+    onInput() {
+        this.dropdownList.classList.add('open');
+
+        let inputValue = this.dropdownInputField.value.toLowerCase();
+        if (inputValue.length > 0) {
+            for (let i = 0; i < this.dropdownListItemsValues.length; i++) {
+                if (!(inputValue.substring(0, inputValue.length) === this.dropdownListItemsValues[i].substring(0, inputValue.length).toLowerCase())) {
+                    this.dropdownListItems[i].classList.add('closed');
+                } else {
+                    this.dropdownListItems[i].classList.remove('closed');
+                }
+            }
+        } else {
+            for (let i = 0; i < this.dropdownListItems.length; i++) {
+                this.dropdownListItems[i].classList.remove('closed');
             }
         }
-    } else {
-        for (let i = 0; i < dropdownListItems.length; i++) {
-            dropdownListItems[i].classList.remove('closed');
+    }
+
+    onMouseDown(e) {
+
+        let isDropdown = this.dropdownList.contains(e.target);
+        let isInput = this.dropdownInputField.contains(e.target);
+
+        if (!isDropdown && !isInput) {
+            this.dropdownList.classList.remove('open');
+        }
+
+        if (e.button === 0) {
+
+            if (e.target.classList.contains('dropdown__item')) {
+                this.previousValue = e.target.textContent;
+                this.dropdownInputField.value = e.target.textContent;
+
+                this.dropdownListItems.forEach(dropdownList => {
+                    dropdownList.classList.add('closed');
+                });
+            }
+
         }
     }
-});
 
-
-dropdownInputField.addEventListener('focus', () => {
-
-    dropdownInputField.placeholder = 'Напиши что-нибудь';
-    dropdownInputField.value = '';
-    dropdownList.classList.add('open');
-    dropdownListItems.forEach(dropdownList => {
-        dropdownList.classList.remove('closed');
-    });
-
-    if ((dropdown.offsetTop) + 220 >= window.innerHeight) {
-        //220 - minimum height of dropdown list
-
-        dropdownList.style.top = 'initial';
-        dropdownList.style.bottom = '0';
-
-    } else {
-
-        dropdownList.style.top = '0';
-        dropdownList.style.bottom = 'initial';
-
-    }
-
-});
-
-dropdownInputField.addEventListener('blur', () => {
-
-    dropdownInputField.placeholder = 'Выбери что-нибудь';
-    dropdownList.classList.remove('open');
-
-    let currentValueMatches = dropdownListItemsValues.filter(function (item) {
-        return item.toLowerCase() == dropdownInputField.value.toLowerCase();
-    });
-
-    if (currentValueMatches.length < 1) {
-        dropdownInputField.value = '';
-
-    }
-    if (previousValue)
-        dropdownInputField.value = previousValue;
-
-});
-
-
-window.addEventListener('resize', function () {
-    dropdownInputField.blur();
-});
-window.addEventListener('scroll', function () {
-    dropdownInputField.blur();
-});
+}
